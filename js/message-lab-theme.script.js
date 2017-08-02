1a4d2ed72a168c0b0dd20f38f9a1096e10a642d9
imperator99 <imperator_99@yahoo.com>
2017-08-02 16:12:05 +1000

    Updated menu style. Added function to JS to check window resizing.

--- js/message-lab-theme.script.js
+++ js/message-lab-theme.script.js
@@ -12,5 +12,39 @@
         });
       });
     }
-  };
+  }
+  Drupal.behaviors.messageLabThemeResize = {
+    // Resize function.
+    attach: function (context, settings) {
+
+      // Check that the correct context exists.
+      if ($(context).find('.nav-toggle').length) {
+        console.log($(this));
+        // Set the toggle width.
+        var showNavToggleAtWidth = 768;
+
+        // Check the width of the window when Drupal loads.
+        $(window).width() < showNavToggleAtWidth ? showNavToggle = true : showNavToggle = false;
+        classToggle(showNavToggle);
+
+        // Check the width of the window when it is resized.
+        $(window).resize(function() {
+          if($(this).width() >= showNavToggleAtWidth) {
+            showNavToggle = false;
+          } else {
+            showNavToggle = true;
+          }
+          classToggle(showNavToggle);
+        });
+        function classToggle(toggle) {
+          console.log(toggle);
+          if (toggle) {
+            $(context).find('.nav-toggle').addClass('active');
+          } else {
+            $(context).find('.nav-toggle').removeClass('active');
+          }
+        }
+      };
+    }
+  }
 })(jQuery, Drupal);
